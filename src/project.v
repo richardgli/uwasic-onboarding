13a9fca5c015fee98aa1fbd20acfc545ca1d6215
Richard Li <rgli@uwaterloo.ca>
2026-08-29 11:06:26 -0400

    Used uo_out output from pwm_peripheral; split spi into three always blocks

--- src/project.v
+++ src/project.v
@@ -35,7 +35,7 @@ module tt_um_uwasic_onboarding_richard_li (
     .en_reg_pwm_7_0(en_reg_pwm_7_0),
     .en_reg_pwm_15_8(en_reg_pwm_15_8),
     .pwm_duty_cycle(pwm_duty_cycle)
-  )
+  );
 
   pwm_peripheral pwm_peripheral_inst (
     .clk(clk),
@@ -47,11 +47,6 @@ module tt_um_uwasic_onboarding_richard_li (
     .pwm_duty_cycle(pwm_duty_cycle),
     .out({uio_out, uo_out})
   );
-
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   // wire _unused = &{ena, clk, rst_n, 1'b0};
--- src/spi_peripheral.v
+++ src/spi_peripheral.v
@@ -1,3 +1,5 @@
+`default_nettype none
+
 module spi_peripheral (
     input  wire COPI,
     input  wire nCS,
@@ -16,7 +18,7 @@ module spi_peripheral (
     reg [15:0] data;
 
     reg COPI1, COPI2;
-    reg nCS1, nCS2;
+    reg nCS1, nCS2, nCS3;
     reg SCLK1, SCLK2, SCLK3;
     reg transaction_ready, transaction_processed;
 
@@ -25,34 +27,43 @@ module spi_peripheral (
             num_bits <= 0;
             COPI2 <= 0;
             COPI1 <= 0;
+            nCS3 <= 0;
             nCS2 <= 0;
             nCS1 <= 0;
             SCLK3 <= 0;
             SCLK2 <= 0;
             SCLK1 <= 0;
-            data <= 0;
-            transaction_ready <= 0;
-        end else if (!nCS2) begin
-            if (nCS1) begin
-                num_bits <= 0;
-            end else if ((SCLK3 && !SCLK2) && num_bits < 5'd16) begin
-                data <= {data[14:0], COPI2};
-                num_bits <= num_bits + 1;
-            end 
-
+        end else begin
             COPI2 <= COPI1;
             COPI1 <= COPI;
+            nCS3 <= nCS2;
             nCS2 <= nCS1;
             nCS1 <= nCS;
             SCLK3 <= SCLK2;
             SCLK2 <= SCLK1;
             SCLK1 <= SCLK;
-        end else begin
-            if (transaction_processed) begin
-                transaction_ready <= 0;
-            end else if (nCS2 && !nCS1) begin
-                transaction_ready <= 1;
-            end
+        end
+    end
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            num_bits <= 0;
+            data <= 0;
+            transaction_ready <= 0;
+            transaction_processed <= 0;
+        end else if (!nCS2 && nCS3) begin
+            num_bits <= 0;
+            transaction_ready <= 0;
+            transaction_processed <= 0;
+        end else if (num_bits < 5'd16 && (SCLK2 && !SCLK3)) begin
+            data <= {data[14:0], COPI2};
+            num_bits <= num_bits + 1;
+        end 
+
+        if (nCS2 && !nCS3) begin
+            transaction_ready <= 1;
+        end else if (transaction_processed) begin
+            transaction_ready <= 0;
         end
     end
 
@@ -63,8 +74,7 @@ module spi_peripheral (
             en_reg_pwm_7_0 <= 0;
             en_reg_pwm_15_8 <= 0;
             pwm_duty_cycle <= 0;
-            transaction_processed <= 0;
-        end else if ((transaction_ready && !transaction_processed) && num_bits == 5'd16 && data[15]) begin
+        end else if ((transaction_ready && !transaction_processed) && num_bits >= 5'd16 && data[15]) begin
             case (data[7:1]) 
                 7'd0: en_reg_out_7_0 <= data[15:8];
                 7'd1: en_reg_out_15_8 <= data[15:8];
@@ -79,3 +89,4 @@ module spi_peripheral (
             transaction_processed <= 0;
         end
     end
+endmodule
